eb6a9f87466f906a1ecb9a8d320853a5bf08e8cb
Suhas S <88608896+SuhasTantri@users.noreply.github.com>
2022-01-22 13:35:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def main():
             else:
                 st.warning("No image uploaded yet")
         except Exception:
-            st.error('Cannot process this file...Please upload a new image.')
+            pass
 
 # calling main function
 main()
